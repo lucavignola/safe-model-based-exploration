187e1126568385b9cb412a5ad545294d253588a9
sukhijab <sukhijab@student.ethz.ch>
2024-09-18 10:24:29 -0700

    merged race car envs with lenart's

--- smbrl/playground/racecar_icem.py
+++ smbrl/playground/racecar_icem.py
@@ -1,3 +1,4 @@
+
 import time
 from typing import Tuple
 
@@ -5,38 +6,144 @@ import chex
 import jax.numpy as jnp
 import jax.random as jr
 import matplotlib.pyplot as plt
+from brax.envs.base import State
+from distrax import Distribution
+from distrax import Normal
+from jaxtyping import Float, Array, Scalar
+from mbpo.systems import DynamicsParams, RewardParams
+from mbpo.systems.base_systems import System, SystemParams, SystemState
+from mbpo.systems.dynamics.base_dynamics import Dynamics
+from mbpo.systems.rewards.base_rewards import Reward
 
-
-from smbrl.envs.racecar import RaceCarSystem, RaceCarEnv
+from smbrl.envs.racecar import RCCar
 from smbrl.optimizer.icem import iCemTO, iCemParams, AbstractCost
 
+DT = 0.03
+MARGIN_FACTOR = 20
+
+
+class DummyDynamics(Dynamics):
+    def __init__(self, x_dim, u_dim):
+        super().__init__(x_dim=x_dim, u_dim=u_dim)
+
+    def next_state(self,
+                   x: chex.Array,
+                   u: chex.Array,
+                   dynamics_params: DynamicsParams) -> Tuple[Distribution, DynamicsParams]:
+        return Normal(0, 0.01), dynamics_params
+
+    def init_params(self, key: chex.PRNGKey) -> DynamicsParams:
+        return 0
+
+
+class DummyReward(Reward):
+    def __init__(self, x_dim, u_dim):
+        super().__init__(x_dim, u_dim)
+
+    def init_params(self, key: chex.PRNGKey) -> RewardParams:
+        return 0
+
+    def __call__(self,
+                 x: chex.Array,
+                 u: chex.Array,
+                 reward_params: RewardParams,
+                 x_next: chex.Array | None = None) -> Tuple[Distribution, RewardParams]:
+        return Normal(0, 0.01), reward_params
+
+
+class CartPoleSystem(System):
+    def __init__(self):
+        super().__init__(dynamics=DummyDynamics(x_dim=5, u_dim=1),
+                         reward=DummyReward(x_dim=5, u_dim=1))
+        self.brax_env = RCCar(dt=DT, margin_factor=MARGIN_FACTOR)
+
+    def step(self,
+             x: chex.Array,
+             u: chex.Array,
+             system_params: SystemParams[DynamicsParams, RewardParams],
+             ) -> SystemState:
+        """
+
+        :param x: current state of the system
+        :param u: current action of the system
+        :param system_params: parameters of the system
+        :return: Tuple of next state, reward, updated system parameters
+        """
+        state = State(pipeline_state=None,
+                      obs=x,
+                      reward=jnp.array(0.0),
+                      done=jnp.array(0.0), )
+
+        next_state = self.brax_env.step(state, u)
+        next_system_state = SystemState(x_next=next_state.obs,
+                                        reward=next_state.reward,
+                                        system_params=system_params,
+                                        done=next_state.done)
+
+        return next_system_state
+
+
+class ActionRepeatWrapper(System):
+    def __init__(self,
+                 action_repeat: int,
+                 system: System):
+        super().__init__(dynamics=system.dynamics, reward=system.reward)
+        self.action_repeat = action_repeat
+        self.system = system
+
+    def step(self,
+             x: chex.Array,
+             u: chex.Array,
+             system_params: SystemParams[DynamicsParams, RewardParams],
+             ) -> SystemState:
+        total_reward = 0.0
+        for _ in range(action_repeat):
+            sys_state = self.system.step(x, u, system_params)
+            x, reward, system_params = sys_state.x_next, sys_state.reward, sys_state.system_params
+            total_reward += reward
+        sys_state = sys_state.replace(reward=total_reward)
+        return sys_state
+
+
+class RadiusBound(AbstractCost):
+
+    def __init__(self, *args, max_radius: float = 2.0, violation_eps=1e-3, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.max_radius = max_radius
+        self.violation_eps = violation_eps
+
+    def __call__(self,
+                 states: Float[Array, 'horizon observation_dim'],
+                 actions: Float[Array, 'horizon action_dim'],
+                 ) -> Scalar:
+        dist_to_origin = jnp.linalg.norm(states[:, :2], axis=-1)
+        trajectory_constraint = jnp.maximum(dist_to_origin - self.max_radius, -self.violation_eps)
+        assert trajectory_constraint.shape == (self.horizon,)
+        return jnp.mean(trajectory_constraint)
 
 
 if __name__ == '__main__':
-    from jax import jit
-
 
     action_repeat = 1
-    horizon = 50
+    horizon = 20
     optimizer = iCemTO(
         horizon=horizon,
-        action_dim=1,
+        action_dim=2,
         key=jr.PRNGKey(0),
-        opt_params=iCemParams(exponent=1.0,
+        opt_params=iCemParams(exponent=2.0,
                               num_samples=500,
-                              num_elites=50,
                               alpha=0.2,
                               num_steps=5,
                               num_particles=1, ),
-        system=RaceCarSystem(),
-        cost_fn=None,
+        system=ActionRepeatWrapper(action_repeat=action_repeat, system=CartPoleSystem()),
+        cost_fn=RadiusBound(horizon=horizon),
     )
 
-    system = RaceCarSystem()
+    system = ActionRepeatWrapper(action_repeat=action_repeat, system=CartPoleSystem())
 
     optimizer_state = optimizer.init(key=jr.PRNGKey(1))
     system_params = system.init_params(key=jr.PRNGKey(2))
-    obs = RaceCarEnv().reset(jr.PRNGKey(0)).obs
+    obs = RCCar(dt=DT, margin_factor=MARGIN_FACTOR).reset(jr.PRNGKey(0)).obs
 
     all_obs = []
     all_actions = []
@@ -44,15 +151,11 @@ if __name__ == '__main__':
 
     times = []
     first_times = time.time()
-
-    step_fn = jit(system.step)
-    act_fn = jit(optimizer.act)
-
-    for i in range(200 // action_repeat):
+    for i in range(100 // action_repeat):
         start_time = time.time()
-        action, optimizer_state = act_fn(obs, optimizer_state)
+        action, optimizer_state = optimizer.act(obs, optimizer_state)
         for _ in range(action_repeat):
-            sys_state = step_fn(obs, action, system_params)
+            sys_state = system.step(obs, action, system_params)
             obs, reward, system_params = sys_state.x_next, sys_state.reward, sys_state.system_params
         all_obs.append(obs)
         all_actions.append(action)
@@ -81,7 +184,7 @@ if __name__ == '__main__':
     print(f'Maximal x position: {jnp.max(jnp.stack(all_obs)[:, 0])}')
     print(f'Minimal x position: {jnp.min(jnp.stack(all_obs)[:, 0])}')
 
-    import numpy as np
+    from smbrl.envs.racecar import plot_rc_trajectory
 
-    total_reward = np.sum(np.array(all_rewards))
-    print(f'Total reward: {total_reward}')
+    all_actions = jnp.stack(all_actions)
+    plot_rc_trajectory(all_obs, all_actions, encode_angle=True)
