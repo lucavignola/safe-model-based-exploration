22a64a3e2310448a67a8bc60ae9c282762c6f389
lenarttreven <lenart.treven44@gmail.com>
2024-09-19 14:00:01 +0200

    clean comments

--- smbrl/envs/cartpole_lenart.py
+++ smbrl/envs/cartpole_lenart.py
@@ -36,14 +36,7 @@ class CartPoleEnv(Env):
         self.dynamics_params = CartPoleDynamicsParams()
         self.reward_params = CartPoleRewardParams()
         self.init_angle = init_angle
-        # bound = 0.1
-        # value_at_margin = 0.1
-        # margin_factor = 10
-        self.reward_source = reward_source  # 'dm-control' or 'gym'
-        # self.tolerance_reward = ToleranceReward(bounds=(0.0, bound),
-        #                                         margin=margin_factor * bound,
-        #                                         value_at_margin=value_at_margin,
-        #                                         sigmoid='long_tail')
+        self.reward_source = reward_source
 
     def reset(self,
               rng: jax.Array) -> State:
@@ -89,18 +82,6 @@ class CartPoleEnv(Env):
                      0] ** 2
         reward = reward.squeeze()
         return reward
-
-    # def dm_reward(self,
-    #               x: Float[Array, 'observation_dim'],
-    #               u: Float[Array, 'action_dim']) -> Float[Array, 'None']:
-    #     theta, omega = jnp.arctan2(x[1], x[0]), x[-1]
-    #     target_angle = self.reward_params.target_angle
-    #     diff_th = theta - target_angle
-    #     diff_th = ((diff_th + jnp.pi) % (2 * jnp.pi)) - jnp.pi
-    #     reward = self.tolerance_reward(jnp.sqrt(self.reward_params.angle_cost * diff_th ** 2 +
-    #                                             0.1 * omega ** 2)) - self.reward_params.control_cost * u ** 2
-    #     reward = reward.squeeze()
-    #     return reward
 
     @partial(jax.jit, static_argnums=0)
     def step(self,
